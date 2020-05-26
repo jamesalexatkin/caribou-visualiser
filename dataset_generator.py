88969d2af1fcbcb73509d7dc3e707bcca04180a5
James Atkin <curlyw42@gmail.com>
2020-05-26 19:24:28 +0100

    Added picker between states and counties

--- dataset_generator.py
+++ dataset_generator.py
@@ -72,11 +72,8 @@ def add_count_to_counties(county_counts, num_in_city, geo_id):
     else:
         county_counts[geo_id] = num_in_city
 
-def write_csv(county_counts):
-    csv_filename = "caribou_dataset_{0}.csv".format(date.today().strftime("%d-%m-%y"))
-    csv_path = os.path.join("datasets", csv_filename)
-
-    with open(csv_path, 'w+') as f:
+def write_csv(county_counts, output_path):
+    with open(output_path, 'w+') as f:
         f.write("%s,%s\n" % ("GEO_ID", "Number"))
         for key in county_counts.keys():
             f.write("%s,%s\n" % (key, county_counts[key]))
@@ -87,13 +84,15 @@ def write_csv(county_counts):
 # Set up arg parser
 parser = argparse.ArgumentParser()
 parser.add_argument("api_key_path", help="Path to file containing API key for Google Maps API")
+parser.add_argument("region_type", choices=['states', 'counties'], help="Whether to produce a map of states or counties")
 
 # Parse args
 args = parser.parse_args()
+api_key_path = args.api_key_path
+region_type = args.region_type
 
 # Read API key from file
-GOOGLE_MAPS_API_KEY = read_api_key(args.api_key_path)
-
+GOOGLE_MAPS_API_KEY = read_api_key(api_key_path)
 
 CARIBOU_BASE_URL = "https://locations.cariboucoffee.com/"
 CARIBOU_LOCATIONS_URL = os.path.join(CARIBOU_BASE_URL, "us")
@@ -106,50 +105,60 @@ html_results = soup.find(id='main')
 
 list_items = html_results.find_all('li', class_='Directory-listItem')
 
-county_counts = {}
 
-COUNTIES_FILE = os.path.join("datasets", "us_counties_500k.json")
-counties_json = json.load(open(COUNTIES_FILE))
+# Sample map at state-level
+if region_type == "states":
+    # TODO
+    pass
 
-for c in counties_json["features"]:
-    geo_id = c["properties"]["GEO_ID"]
-    # r = random.randint(0, 10)
-    add_count_to_counties(county_counts, "", geo_id)
+# Sample map at county-level
+elif region_type == "counties":
 
-for li in tqdm(list_items):
-    list_link = li.find('a')
+    county_counts = {}
 
-    state_name = list_link.text
-    state_link = list_link['href']
+    COUNTIES_FILE = os.path.join("datasets", "us_counties_500k.json")
+    counties_json = json.load(open(COUNTIES_FILE))
 
-    # Extract number of stores in state from HTML to an int
-    num_in_state = format_html_number(li.find('span').text)
+    for c in counties_json["features"]:
+        geo_id = c["properties"]["GEO_ID"]
+        add_count_to_counties(county_counts, "", geo_id)
 
-    # Edge case where states only have one store
-    # Caribou website has the link directly to the store, rather than the state
-    if num_in_state == 1:
-        state_link = convert_to_base_state_link(state_link)
+    for li in tqdm(list_items):
+        list_link = li.find('a')
+
+        state_name = list_link.text
+        state_link = list_link['href']
+
+        # Extract number of stores in state from HTML to an int
+        num_in_state = format_html_number(li.find('span').text)
+
+        # Edge case where states only have one store
+        # Caribou website has the link directly to the store, rather than the state
+        if num_in_state == 1:
+            state_link = convert_to_base_state_link(state_link)
 
 
-    state_page_url = os.path.join(CARIBOU_BASE_URL, state_link)
-    state_page = requests.get(state_page_url)
-    state_soup = BeautifulSoup(state_page.content, 'html.parser')
-    state_html_results = state_soup.find(id='main')
+        state_page_url = os.path.join(CARIBOU_BASE_URL, state_link)
+        state_page = requests.get(state_page_url)
+        state_soup = BeautifulSoup(state_page.content, 'html.parser')
+        state_html_results = state_soup.find(id='main')
 
-    city_list_items = state_html_results.find_all('li', class_='Directory-listItem')
+        city_list_items = state_html_results.find_all('li', class_='Directory-listItem')
 
-    for city_li in city_list_items:
-        city_list_link = city_li.find('a')
+        for city_li in city_list_items:
+            city_list_link = city_li.find('a')
 
-        city_name = city_list_link.text     
+            city_name = city_list_link.text     
 
-        # Extract number of stores in city from HTML to an int
-        num_in_city = format_html_number(city_li.find('span').text)
+            # Extract number of stores in city from HTML to an int
+            num_in_city = format_html_number(city_li.find('span').text)
 
-        county_of_city = find_county_of_city(city_name, state_name)
+            county_of_city = find_county_of_city(city_name, state_name)
 
-        geo_id = find_geo_id(county_of_city, state_name, counties_json)
-        
-        add_count_to_counties(county_counts, num_in_city, geo_id)
+            geo_id = find_geo_id(county_of_city, state_name, counties_json)
+            
+            add_count_to_counties(county_counts, num_in_city, geo_id)
 
-write_csv(county_counts)
+    output_filename = "caribou_" + region_type + "_dataset_" + date.today().strftime("%d-%m-%y") + ".html"
+    output_path = os.path.join('./datasets/', output_filename)
+    write_csv(county_counts, output_path)
--- visualiser.py
+++ visualiser.py
@@ -10,33 +10,52 @@ import pandas as pd
 # Set up arg parser
 parser = argparse.ArgumentParser()
 parser.add_argument("caribou_dataset_path", help="Path to CSV dataset for number of Caribou dataset")
+parser.add_argument("region_type", choices=['states', 'counties'], help="Whether to produce a map of states or counties")
 
 # Parse args
 args = parser.parse_args()
-
-# Load the shape of US counties
-county_geo_filename = os.path.join('./datasets', 'us_counties_500k.json') # from http://eric.clst.org/Stuff/USGeoJSON
+num_stores = args.caribou_dataset_path
+region_type = args.region_type
 
 # Load the Caribou dataset
-num_stores = args.caribou_dataset_path
 county_data = pd.read_csv(num_stores)
 
 # Initialize the map:
 m = folium.Map(location=[38, -103], zoom_start=5)
 
-folium.Choropleth(
-    geo_data=county_geo_filename,
-    data=county_data,
-    # columns=['County', ' Number'],
-    columns=['GEO_ID','Number'],
-    key_on='properties.GEO_ID',
-    # bins=11,
-    fill_color='YlGnBu',
-    fill_opacity=0.8,
-    line_weight=1,
-    legend_name='Number of Caribou Coffee Stores'
-).add_to(m)
+# Map at state-level
+if region_type == "states":
+    # Load the shape of US states
+    states_geo_filename = os.path.join('./datasets', 'us_states.json')
+
+    folium.Choropleth(
+        geo_data=states_geo_filename,
+        data=county_data,
+        columns=['id','Number'],
+        key_on='feature.id',
+        fill_color='YlGnBu',
+        fill_opacity=0.8,
+        line_weight=1,
+        legend_name='Number of Caribou Coffee Stores'
+    ).add_to(m)
+
+# Map at county-level
+elif region_type == "counties":
+    # Load the shape of US counties
+    county_geo_filename = os.path.join('./datasets', 'us_counties_500k.json') # from http://eric.clst.org/Stuff/USGeoJSON
+
+    folium.Choropleth(
+        geo_data=county_geo_filename,
+        data=county_data,
+        columns=['GEO_ID','Number'],
+        key_on='properties.GEO_ID',
+        fill_color='YlGnBu',
+        fill_opacity=0.8,
+        line_weight=1,
+        legend_name='Number of Caribou Coffee Stores'
+    ).add_to(m)
  
 # Save to html file
-output_filename = "caribou_map_" + date.today().strftime("%d-%m-%y") + ".html"
-m.save(output_filename)
+output_filename = "caribou_" + region_type + "_map_" + date.today().strftime("%d-%m-%y") + ".html"
+output_path = os.path.join('./results/', output_filename)
+m.save(output_path)
